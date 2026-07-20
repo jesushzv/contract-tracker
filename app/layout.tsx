7d06aa4b6ae13e01e844f62f3f1c50cf73f722bb
Hector Zamora <jhector.zamora@hotmail.com>
2026-07-20 13:36:56 -0700

    feat: add Meta Pixel tracking script to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit, Inter } from "next/font/google";
+import Script from "next/script";
 import { cookies } from "next/headers";
 import Header from "./Header";
 import Footer from "./Footer";
@@ -54,6 +55,29 @@ export default async function RootLayout({
       className={`${outfit.variable} ${inter.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-[#fafbfe] text-[#0f172a] transition-colors duration-300">
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '812279541878360');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=812279541878360&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <ApiKeyGuard>
           <Header hasAuthCookie={hasAuthCookie} useSupabase={useSupabase} />
 
